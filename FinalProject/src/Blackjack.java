f857bd99ffc9aa522f53be660e469072d17f6ffc
Scott Caratozzolo <scaratozzolo12@gmail.com>
2016-12-15 21:45:17 -0500

    Almost done
    added more functionality - check user's bet against their money, allow user to choose value of their ace, and working on finishing touches for checking the winner/blackjack/bust

--- FinalProject/src/Blackjack.java
+++ FinalProject/src/Blackjack.java
@@ -18,19 +18,22 @@ public class Blackjack {
 	
 	static int user;
 	static int dealer;
+	static double bet;
+	static double currentCash;
+	static double totalWinnings;
 	
 	static Cards cards = new Cards();
 	
 	static boolean keepPlaying = true; //used for while loop to keep playing
 	static boolean playing = true; //used for do while loop for each time a card is added to user's hand
 	
+	
 	public static void main(String[] args) {
 		
 		boolean cont = false; //used for do while loop to ask if the user wants to keep playing
+		boolean checkMoney = true;
 		final double startingCash;
-		double currentCash;
-		double bets = 0;
-		int total;
+
 		
 		currentDeckFace = new ArrayList<String>();
 		currentDeckValue = new ArrayList<Integer>();
@@ -49,26 +52,36 @@ public class Blackjack {
 		currentCash = startingCash;
 		keyboard.nextLine(); //consumes rest of line
 		
+		//loop that allows the user to play again
 		while (keepPlaying) {
+			
 			initializeCards();	//clears old deck to create a new one
+			
+			
+			do {	//loop to make sure the user doesn't bet more then they have
+				System.out.println("How much money would you like to bet? Enter the amount:");
+				bet = keyboard.nextDouble();
+				keyboard.nextLine();	//consumes rest of line
+				if (bet > currentCash) {
+					System.out.println("You don't have enough money for that bet!");
+					checkMoney = true;
+				} else {
+					currentCash -= bet;
+					checkMoney = false;
+				}
+			} while (checkMoney);
+			
 			dealStartCards();	//deals 2 cards to the user and the dealer
 			
-			System.out.println("How much money would you like to bet? Enter the amount:");
-			double bet = keyboard.nextDouble();
-			keyboard.nextLine();		//TODO check user's money
-			currentCash -= bet;
-			bets += bet;
 			
-			do {
+			do {	//loop for the actual game
 				
-				total = userTotal();
-				System.out.println("Your cards are " + userFace + " which totals " + total + ".");
+				userCards();
+				System.out.println();
+				dealerShowing();
+				standHit();		//TODO check blackjack before standhit
+				blackjackBust();
 				
-				System.out.println("Do you want to Stand or Hit? Enter 1 for stand or 2 for Hit:");
-				int input = keyboard.nextInt();
-				keyboard.nextLine();
-				
-				standHit(input);
 	
 			}while (playing);
 			
@@ -97,6 +110,8 @@ public class Blackjack {
 			}while (cont);
 		}
 		
+		
+		
 	}
 	
 	//prints out starting instructions
@@ -115,8 +130,11 @@ public class Blackjack {
     	char letter = lower.charAt(0);
     	int lowerLength = lower.length();
     	
-    		
-    	if (lower.equals("yes") || (letter == 'y' && lowerLength == 1)) {
+    	if (currentCash <= 0) {
+			System.out.println("I'm sorry, you have no more money to continue playing.");
+			keepPlaying = false;
+			return "no";
+		} else if (lower.equals("yes") || (letter == 'y' && lowerLength == 1)) {
     		return "yes";
     	} else if (lower.equals("no") || (letter == 'n' && lowerLength == 1)) {
     		return "no";
@@ -131,18 +149,38 @@ public class Blackjack {
 	public static void dealStartCards() {
 		
 		int value;
+		int total = userTotal();
+		boolean aceTest1 = false; //used to keep looping until user enters a correct Ace value
+		boolean aceTest2 = false; //used to keep looping until user enters a correct Ace value
 		
 		//picks user's first card
 		cards.setCards(currentDeckFace.size());
 		int user1 = cards.getPick();
 		if (currentDeckValue.get(user1) == 0) {
+			System.out.println("Your cards: " + userFace + " which totals " + total + ".");
 			System.out.println("You have an ace! Would you like to use it as a 1 or an 11? Enter 1 or 11:"); 
-			int input = keyboard.nextInt();	//todo check if 1 or 11
-			keyboard.nextLine();
-			userFace.add(currentDeckFace.get(user1));
-			userValue.add(input);
-			currentDeckFace.remove(user1);
-			currentDeckValue.remove(user1);
+			do { 	//tests if user entered 1 or 11
+				
+				int input = keyboard.nextInt();	
+				keyboard.nextLine(); //consumes rest of line
+				
+				if (input == 1 ) {
+					userFace.add(currentDeckFace.get(user1));
+					userValue.add(input);
+					currentDeckFace.remove(user1);
+					currentDeckValue.remove(user1);
+					aceTest1 = false;
+				} else if (input == 11) {
+					userFace.add(currentDeckFace.get(user1));
+					userValue.add(input);
+					currentDeckFace.remove(user1);
+					currentDeckValue.remove(user1);
+					aceTest1 = false;
+				} else {
+					System.out.println("Please enter a correct value!");
+				}
+				
+			} while (aceTest1);
 		} else {
 			userFace.add(currentDeckFace.get(user1));
 			userValue.add(currentDeckValue.get(user1));
@@ -176,13 +214,30 @@ public class Blackjack {
 		cards.setCards(currentDeckFace.size());
 		int user2 = cards.getPick();
 		if (currentDeckValue.get(user2) == 0) {
-			System.out.println("You have an ace! Would you like to use it as a 1 or an 11? Enter 1 or 11:");
-			int input = keyboard.nextInt(); //todo check if 1 or 11
-			keyboard.nextLine();
-			userFace.add(currentDeckFace.get(user2));
-			userValue.add(input);
-			currentDeckFace.remove(user2);
-			currentDeckValue.remove(user2);
+			System.out.println("Your cards: " + userFace + " which totals " + total + ".");
+			System.out.println("You have an ace! Would you like to use it as a 1 or an 11? Enter 1 or 11:"); 
+			do {	//tests if user entered 1 or 11
+				
+				int input = keyboard.nextInt();	
+				keyboard.nextLine();	//consumes rest of line
+				
+				if (input == 1 ) {
+					userFace.add(currentDeckFace.get(user1));
+					userValue.add(input);
+					currentDeckFace.remove(user1);
+					currentDeckValue.remove(user1);
+					aceTest2 = false;
+				} else if (input == 11) {
+					userFace.add(currentDeckFace.get(user1));
+					userValue.add(input);
+					currentDeckFace.remove(user1);
+					currentDeckValue.remove(user1);
+					aceTest2 = false;
+				} else {
+					System.out.println("Please enter a correct value!");
+				}
+			} while (aceTest2);
+			
 		} else {
 			userFace.add(currentDeckFace.get(user2));
 			userValue.add(currentDeckValue.get(user2));
@@ -239,17 +294,56 @@ public class Blackjack {
 		return total;
 	}
 	
+	public static int dealerTotal() {
+		int total = 0;
+		for (int i = 0; i < dealerValue.size(); i++) {
+			total = total + dealerValue.get(i);
+		}
+		
+		return total;
+	}
+	
+	public static int dealerShowingTotal() {
+		int showing = 0;
+		for (int i = 1; i < dealerValue.size(); i++) {
+			showing = showing + dealerValue.get(i);
+		}
+		
+		return showing;
+	}
+	
 	public static void dealUserCard() {
+		
+		int total = userTotal();
+		boolean aceTest = true;
+		
 		cards.setCards(currentDeckFace.size());
 		int user1 = cards.getPick();
 		if (currentDeckValue.get(user1) == 0) {
-			System.out.println("You have an ace! Would you like to use it as a 1 or an 11? Enter 1 or 11:");
-			int input = keyboard.nextInt(); //todo check if 1 or 11
-			keyboard.nextLine();
-			userFace.add(currentDeckFace.get(user1));
-			userValue.add(input);
-			currentDeckFace.remove(user1);
-			currentDeckValue.remove(user1);
+			System.out.println("Your cards: " + userFace + " which totals " + total + ".");
+			System.out.println("You have an ace! Would you like to use it as a 1 or an 11? Enter 1 or 11:"); 
+			do {	//tests if user entered 1 or 11
+				
+				int input = keyboard.nextInt();	
+				keyboard.nextLine(); //consumes rest of line
+				
+				if (input == 1 ) {
+					userFace.add(currentDeckFace.get(user1));
+					userValue.add(input);
+					currentDeckFace.remove(user1);
+					currentDeckValue.remove(user1);
+					aceTest = false;
+				} else if (input == 11) {
+					userFace.add(currentDeckFace.get(user1));
+					userValue.add(input);
+					currentDeckFace.remove(user1);
+					currentDeckValue.remove(user1);
+					aceTest = false;
+				} else {
+					System.out.println("Please enter a correct value!");
+				}
+				
+			} while (aceTest);
 		} else {
 			userFace.add(currentDeckFace.get(user1));
 			userValue.add(currentDeckValue.get(user1));
@@ -284,11 +378,17 @@ public class Blackjack {
 		}
 	}
 	
-	public static void standHit(int value) {
-		if (value == 1) {
-			playing  = false;
+	public static void standHit() {
+		System.out.println();
+		System.out.println("Do you want to Stand or Hit? Enter 1 for stand or 2 for Hit:");
+		int input = keyboard.nextInt();
+		keyboard.nextLine();
+		
+		if (input == 1) {
+			stand();
 		} else {
 			dealUserCard();
+			dealDealerCard();
 		}
 	}
 	
@@ -298,11 +398,120 @@ public class Blackjack {
 			System.out.println("Five Card Charlie! Your cards were " + userFace + " and totaled " + userTotal() + ".");
 		}
 	}
+	
+	public static void userCards() {
+		int total = userTotal();
+		
+		System.out.println("Your cards are: ");
+		for (int i = 0; i < userFace.size(); i++) {
+			System.out.println(userFace.get(i));
+		}
+		
+		System.out.println("That totals " + total + ".");
+	}
+	
+	public static void dealerShowing() {
+		int showing = dealerShowingTotal();
+		
+		System.out.println("The dealer has: ");
+		
+		for (int i = 1; i < dealerFace.size(); i++) {
+			System.out.println(dealerFace.get(i));
+		}
+		
+		System.out.println("The dealer has " + showing + " showing.");
+	}
+	
+	public static void stand() {
+		playing  = false;
+		dealDealerCard();
+	}
+	
+	public static void checkWinner(String result) {
+		
+		int user = userTotal();
+		int dealer = dealerTotal();
+		
+		if (result == "null") {
+			if (user > dealer) {
+				double winnings = bet * 2;
+				currentCash += winnings;
+				totalWinnings += winnings;
+				System.out.println("Your cards " + userFace + " which total " + user);
+				System.out.println("The dealer's cards " + dealerFace + " which total " + dealer);
+				System.out.println("YOU WON! Your winnings are " + winnings + " which brings your current cash to " + currentCash + ".");
+			} else if (user < dealer) {
+				double losses = bet * 2;
+				currentCash -= losses;
+				totalWinnings -= losses;
+				System.out.println("Your cards " + userFace + " which total " + user);
+				System.out.println("The dealer's cards " + dealerFace + " which total " + dealer);
+				System.out.println("YOU LOSE! Your losses are " + losses + " which brings your current cash to " + currentCash + ".");
+			} else if (user == dealer) {
+				double losses = bet * 2;
+				currentCash -= losses;
+				totalWinnings -= losses;
+				System.out.println("Your cards " + userFace + " which total " + user);
+				System.out.println("The dealer's cards " + dealerFace + " which total " + dealer);
+				System.out.println("IT'S A TIE, YOU LOSE! Your losses are " + losses + " which brings your current cash to " + currentCash + ".");
+			}
+		} else if (result == "userBlackjack") {
+			double winnings = bet * 4;
+			currentCash += winnings;
+			totalWinnings += winnings;
+			System.out.println("Your cards " + userFace + " which total " + user);
+			System.out.println("The dealer's cards " + dealerFace + " which total " + dealer);
+			System.out.println("YOU WON! Your winnings are " + winnings + " which brings your current cash to " + currentCash + ".");
+		} else if (result == "dealerBlackjack") {
+			double losses = bet * 4;
+			currentCash -= losses;
+			totalWinnings -= losses;
+			System.out.println("Your cards " + userFace + " which total " + user);
+			System.out.println("The dealer's cards " + dealerFace + " which total " + dealer);
+			System.out.println("YOU LOSE! Your losses are " + losses + " which brings your current cash to " + currentCash + ".");
+		} else if (result == "userBusted") {
+			double losses = bet * 3;
+			currentCash -= losses;
+			totalWinnings -= losses;
+			System.out.println("Your cards " + userFace + " which total " + user);
+			System.out.println("The dealer's cards " + dealerFace + " which total " + dealer);
+			System.out.println("YOU LOSE! Your losses are " + losses + " which brings your current cash to " + currentCash + ".");	
+		} else if (result == "dealerBusted") {
+			double winnings = bet * 3;
+			currentCash += winnings;
+			totalWinnings += winnings;
+			System.out.println("Your cards " + userFace + " which total " + user);
+			System.out.println("The dealer's cards " + dealerFace + " which total " + dealer);
+			System.out.println("YOU WON! Your winnings are " + winnings + " which brings your current cash to " + currentCash + ".");
+		} else {
+			System.out.println("Broken");
+		}
+	}
+	
+	public static void blackjackBust() {
+		int userTotal = userTotal();
+		int dealerTotal = dealerTotal();
+		
+		if (userFace.size() == 2 && userTotal == 21) {
+			System.out.println("BLACKJACK!");
+			checkWinner("userBlackjack");
+		} else if (dealerFace.size() == 2 && dealerTotal == 21){
+			System.out.println("The dealer got Blackjack!");
+			checkWinner("dealerBlackjack");
+		} else if (userTotal > 21) {
+			System.out.println("You busted!");
+			checkWinner("userBusted");
+		} else if (dealerTotal > 21) {
+			System.out.println("The dealer busted!");
+			checkWinner("dealerBusted");
+		} else {
+			checkWinner("null");
+		}
+		
+	}
 }
 
 /* TODO
- * check players bet to their money
- * check if user entered 1 or 11 on ace
  * check who won
  * 
  */
